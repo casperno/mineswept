059f66065139ec86e309f8a655e8d5992ecf9ac1
Torbjørn Caspersen <torbjorn.caspersen@gmail.com>
2021-08-09 19:03:14 +0200

    click events

--- src/Game.ts
+++ src/Game.ts
@@ -1,40 +1,56 @@
 export class Game {
+  private demoMode = true;
+  private cols: number;
+  private rows: number;
 
-    private demoMode = true;
+  constructor() {
+    const boardContainer = document.getElementById("board");
+    this.generateBoard(boardContainer, 20, 20);
+    console.log("main");
+  }
 
-    constructor() {
-        const boardContainer = document.getElementById('board');
-        this.generateBoard(boardContainer, 20, 20);
-        console.log("main");
+  private generateBoard(target: HTMLElement, cols: number, rows: number) {
+    this.cols = cols;
+    this.rows = rows;
+
+    (
+      target as HTMLDivElement
+    ).style.gridTemplateColumns = `repeat(${cols}, 29px)`;
+
+    const numCells = rows * cols;
+    for (let i = 0; i < numCells; i++) {
+      target.append(this.createCell(i));
     }
+  }
 
-    private generateBoard(target: HTMLElement, cols: number, rows: number,) {
-        (target as HTMLDivElement).style.gridTemplateColumns = `repeat(${cols}, 27px)`;
-
-        for (let i = rows * cols; i > 0; i--) {
-            target.appendChild(this.createCell());
+  private createCell(index: number) {
+    const div = document.createElement("div");
+    div.className = "cell open";
+    div.setAttribute("i", index.toString());
+    if (this.demoMode) {
+      if (Math.random() > 0.2) {
+        if (Math.random() > 0.2) {
+          const icon = Math.random() > 0.5 ? "icon-flag" : "icon-bomb";
+          div.innerHTML = `<i class='${icon}'></i>`;
+        } else {
+          div.innerHTML = `<span class='count'>${Math.ceil(
+            Math.random() * 7
+          ).toString()}</span>`;
         }
-
+      }
     }
 
-    private createCell() {
-        const div = document.createElement('div');
-        div.className = "cell";
+    div.addEventListener("click", (e: MouseEvent) => {
+      const index = parseInt(
+        (e.currentTarget as HTMLElement).getAttribute("i")
+      );
+      console.log("click index ", index, this.getCoordinates(index));
+    });
 
-        if (this.demoMode) {
+    return div;
+  }
 
-            if (Math.random() > .2) {
-
-                if (Math.random() > .2) {
-                    const icon = Math.random() > .5 ? 'icon-flag' : 'icon-bomb';
-                    div.innerHTML = `<i class='${icon}'></i>`
-                } else {
-
-                    div.innerHTML = `<span class='count'>${Math.ceil(Math.random() * 7).toString()}</span>`;
-                }
-            }
-        }
-
-        return div;
-    }
+  private getCoordinates(index: number) {
+    return { col: index % this.cols, row: Math.floor(index / this.rows) };
+  }
 }
