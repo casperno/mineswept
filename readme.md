5d0f8993bf1b2c90f7c428f8d3fe907628b822a0
Torbjørn Caspersen <casper@papirfly.no>
2024-01-12 13:24:37 +0100

    optional autosolver

--- readme.md
+++ readme.md
@@ -4,7 +4,8 @@ A training excercise in building the old classic, Minesweeper, in typescript.
 
 Additionally, a step by step autosolver, where the code plays the game by itself, showing the steps.
 
-Try the game [here](/built/index.html)
+Try the game [here](/built/index.html)  
+See the autosover in action [here](/built/index.html?auto=true)
 
 Uses a custom font for icons for bomb and flag. `src/res/fontello.woff`
 
--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,18 @@ function init() {
   // init a standard game
   let game = new Game(document.getElementById("board"));
 
-  // COMMENT OUT Autosolver to play game manually.
-  // set up autosolver of game, hook it upp with a place to output log
-  //  and a button element for progressing
-  let autosolver = new Autosolver(
-    game,
-    document.getElementById("log"),
-    document.getElementById("next")
-  );
+  const url = new URL(window.location.href);
+  const auto = url.searchParams.get("auto");
+
+  if (auto.toLowerCase() == "true") {
+    // Autosolver to play game manually.
+    // set up autosolver of game, hook it upp with a place to output log
+    // and a button element for progressing
+    let autosolver = new Autosolver(
+      game,
+      document.getElementById("log"),
+      document.getElementById("next")
+    );
+  }
 }
 init();
